e6305011f220b9be9bb02b135c83fe07ba6cd096
NTALASHA TEDDY <ziwamorentalasha91@gmail.com>
2026-06-14 15:21:33 +0200

    fix: import AuthModule in SystemCommunicationsModule for SuperAdminGuard dependency

--- backend/src/system-communications/system-communications.module.ts
+++ backend/src/system-communications/system-communications.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SystemCommunicationsController } from './system-communications.controller';
 import { SystemCommunicationsService } from './system-communications.service';
 import { EmailModule } from '../email/email.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [AuthModule, EmailModule],
   controllers: [SystemCommunicationsController],
   providers: [SystemCommunicationsService],
   exports: [SystemCommunicationsService],
